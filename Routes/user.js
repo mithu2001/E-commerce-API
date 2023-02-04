dc7623047dad47f87edf67f1446158fe9416dfdf
mithu2001 <guptamithu2019@gmail.com>
2023-02-04 08:24:28 +0530

    make a user Router

---
+++ Routes/user.js
@@ -0,0 +1,15 @@
+const router = require('express').Router; //Require Router from express
+const User = require('../models/user'); //Require model of DB
+//CRUD OPERATION
+//Create User and save into DB
+
+
+//Delete single User
+//Delete all User
+
+//Get a single user
+//Get all user
+
+//update user
+
+module.exports = router;
